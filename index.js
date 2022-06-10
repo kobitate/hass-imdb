0d4c3305119c30b4472fee4d2514f51e615cea53
Kobi Tate <kobi@kobitate.com>
2022-06-10 16:41:17 -0400

    Initial functional commit

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const {
+  HASS_TOKEN,
+  HASS_ENTITY,
+  HASS_HOST,
+} = process.env
+
+const hassConfig = {
+  headers: {
+    Authorization: `Bearer ${HASS_TOKEN}`,
+    'Content-Type': 'application/json'
+  }
+}
+
+app.get('/', (req, res) => {
+
+  const hassUrl = `${HASS_HOST}/api/states/${HASS_ENTITY}`
+
+  axios.get(hassUrl, hassConfig).then(hassRes => {
+    const { attributes }  = hassRes.data
+
+    const type = attributes.media_content_type || 'nothing'
+    let query
+
+    switch(type) {
+      case 'nothing':
+        res.send(`Nothing playing on ${attributes.friendly_name}`)
+        break
+      case 'tvshow':
+        const show = attributes.media_series_title.replaceAll(' ','+')
+        const season = attributes.media_season || 1
+        const episode = attributes.media_episode || 1
+        const episodeTitle = attributes.media_title.replaceAll(' ','+')
+        query = `${show}+season+${season}+episode+${episode}+${episodeTitle}`
+        break
+      case 'movie':
+        query = attributes.media_title.replaceAll(' ', '+')
+        break
+      default:
+        res.send('Incompatible media type \'${type}\', please play a TV Show or Movie')
+    }
+
+    if (query !== undefined) {
+      const url = `https://html.duckduckgo.com/html?q=\\${query}+site%3Aimdb.com`
+      res.redirect(url)
+    } else {
+      res.send('Something went wrong')
+    }
+
+  }).catch(e => {
+    console.error(e)
+    res.send(e)
+  })
+
+})
+
+app.listen(port, () => {
+  console.log('hass-imdb is listening...')
+})
