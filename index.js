33546cb50399050f9fad6bba42d587c9eace15fe
Kobi Tate <kobi@veobot.com>
2022-06-27 15:03:14 -0400

    standardjs fixes, installed standard package

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 const {
   HASS_TOKEN,
   HASS_ENTITY,
-  HASS_HOST,
+  HASS_HOST
 } = process.env
 
 const hassConfig = {
@@ -20,31 +20,33 @@ const hassConfig = {
 }
 
 app.get('/', (req, res) => {
-
   const hassUrl = `${HASS_HOST}/api/states/${HASS_ENTITY}`
 
   axios.get(hassUrl, hassConfig).then(hassRes => {
-    const { attributes }  = hassRes.data
+    const { attributes } = hassRes.data
 
     const type = attributes.media_content_type || 'nothing'
     let query
 
-    switch(type) {
-      case 'nothing':
+    switch (type) {
+      case 'nothing': {
         res.send(`Nothing playing on ${attributes.friendly_name}`)
         break
-      case 'tvshow':
-        const show = attributes.media_series_title.replaceAll(' ','+')
+      }
+      case 'tvshow': {
+        const show = attributes.media_series_title.replaceAll(' ', '+')
         const season = attributes.media_season || 1
         const episode = attributes.media_episode || 1
-        const episodeTitle = attributes.media_title.replaceAll(' ','+')
+        const episodeTitle = attributes.media_title.replaceAll(' ', '+')
         query = `${show}+season+${season}+episode+${episode}+${episodeTitle}`
         break
-      case 'movie':
+      }
+      case 'movie': {
         query = attributes.media_title.replaceAll(' ', '+')
         break
+      }
       default:
-        res.send('Incompatible media type \'${type}\', please play a TV Show or Movie')
+        res.send(`Incompatible media type ${type}, please play a TV Show or Movie`)
     }
 
     if (query !== undefined) {
@@ -53,12 +55,10 @@ app.get('/', (req, res) => {
     } else {
       res.send('Something went wrong')
     }
-
   }).catch(e => {
     console.error(e)
     res.send(e)
   })
-
 })
 
 app.listen(port, () => {
